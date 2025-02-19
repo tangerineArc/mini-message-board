b87ca5dd787529f35a88becff0f0a807e9014c92
Swagatam <swagatamheroz@outlook.com>
2025-02-19 22:35:32 +0530

    feat: add client-side form validation

---
+++ public/validate-form.js
@@ -0,0 +1,18 @@
+(() => {
+  const userInputField = document.querySelector("#user");
+  const messageInputField = document.querySelector("#text");
+
+  document.querySelector("form").addEventListener("submit", (event) => {
+    if (!userInputField.value.trim() || !messageInputField.value.trim()) {
+      event.preventDefault();
+
+      if (!userInputField.value.trim()) {
+        document.querySelector(".r1").style.visibility = "visible";
+      }
+
+      if (!messageInputField.value.trim()) {
+        document.querySelector(".r2").style.visibility = "visible";
+      }
+    }
+  });
+})();
